ef366b9f859e507a6c6330c343246aa5d825e1a5
Maximovams <wk1342@gmail.com>
2023-12-13 23:03:42 +0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,14 @@
+headers = {
+  "Content-Type": "application/json"
+}
+
+user_body = {
+  "firstName": "Маруся",
+  "phone": "+79517115539",
+  "address": "г. Смоленск, ул. Колотушкина, д. 1"
+}
+
+kit_body = {
+  "name": "Суббота"
+ }
+
