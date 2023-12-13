8187efef80e3b434cd32a71993804f1132529757
Maximovams <wk1342@gmail.com>
2023-12-13 23:03:36 +0300

    Create configuration.py

---
+++ configuration.py
@@ -0,0 +1,5 @@
+BASE_URL = "https://0f6fb58f-7a11-47b9-b11d-e2b2a99142e3.serverhub.praktikum-services.ru"
+MAIN_USER = "/api/v1/users/"
+USER_MODEL = "/api/db/resources/user_model.csv"
+MAIN_USER_KIT = "/api/v1/kits/"
+
