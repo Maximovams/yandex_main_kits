43aa1630878af72914fe10a5db05cf6a3a404f01
Maximovams <wk1342@gmail.com>
2023-12-14 17:49:07 +0300

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -9,9 +9,6 @@ def post_new_user(body):
                          json=body,
                          headers=data.headers)
 response = post_new_user(data.user_body)
-print(response.status_code)
-print(response.json())
-
 
 def post_new_client_kit(kit_body):
     headers_dict = data.headers.copy()
@@ -20,5 +17,3 @@ def post_new_client_kit(kit_body):
                          json=kit_body,
                          headers=headers_dict)
 response = post_new_client_kit(data.kit_body)
-print(response.status_code)
-print(response.json())
